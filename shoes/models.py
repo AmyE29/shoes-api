fe39c246f3133af49c672edbd214df32c398a6d5
Amy Evans <amykarline@yahoo.com>
2020-01-21 21:28:30 -0800

    done

--- shoes/models.py
+++ shoes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 class Shoes(models.Model):
+    
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     brand = models.CharField(max_length=100)
     style = models.TextField()
--- shoes/urls.py
+++ shoes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ShoesList, ShoesDetail
+from .views import ShoesList, ShoesDetail 
 
 urlpatterns = [
     path('shoe/', ShoesList.as_view(), name= 'shoes_list'),
     path('shoe/<int:pk>/', ShoesDetail.as_view(), name= 'shoes_detail')
+
 ]
